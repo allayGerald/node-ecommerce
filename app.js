8326d3b59fb8256141b33419593e00fd512b3779
allayDlareg <geraldallay@gmail.com>
2019-04-16 17:54:28 +0300

    Understanding url

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const http = require('http');
 
 http.createServer((req, res) => {
 res.setHeader('Content-Type', 'text/html');
+const url = req.url;
+if(url === '/'){
+    res.write('<html>');
+    res.write('<head ><h1 style="color:red">THE HEAD</h1></head>');
+    res.write('<body><form method="POST" action="/message"><input type="text" name="message"/><button>Send</button></form></body>');
+    res.write('</html>');
+    return res.end(); 
+}
 res.write('<html>');
 res.write('<head ><h1 style="color:red">THE HEAD</h1></head>');
 res.write('<body>Hello World</body>');
