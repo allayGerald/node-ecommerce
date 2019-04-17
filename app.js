5f9f6eebd91b56e179efc13dd8a781b9be5c51d1
allayDlareg <geraldallay@gmail.com>
2019-04-17 20:57:55 +0300

    manage redirects and play with fs

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
 res.setHeader('Content-Type', 'text/html');
 const url = req.url;
+const method = req.method;
 if(url === '/'){
     res.write('<html>');
     res.write('<head ><h1 style="color:red">THE HEAD</h1></head>');
     res.write('<body><form method="POST" action="/message"><input type="text" name="message"/><button>Send</button></form></body>');
     res.write('</html>');
     return res.end(); 
+}
+
+if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.pdf', 'Dummy');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
 }
 res.write('<html>');
 res.write('<head ><h1 style="color:red">THE HEAD</h1></head>');
