a247411f9938b194766e44a0cd3775f873440d17
allayDlareg <geraldallay@gmail.com>
2019-04-20 02:21:50 +0300

    Middleware

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('First Middleware');
+    next(); // Allows calling next middleware
+})
+
+app.use((req, res, next) => {
+    res.send('<h1>EXPRESS!!</h1>');
+})
+
 http.createServer(app).listen('3000');
