e3edfdc2202af72d0335965d02119869d115e263
allayDlareg <geraldallay@gmail.com>
2019-04-19 22:38:32 +0300

    Exporting Node modules

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-http.createServer((req, res) => {
-res.setHeader('Content-Type', 'text/html');
-const url = req.url;
-const method = req.method;
-if(url === '/'){
-    res.write('<html>');
-    res.write('<head ><h1 style="color:red">THE HEAD</h1></head>');
-    res.write('<body><form method="POST" action="/message"><input type="text" name="message"/><button>Send</button></form></body>');
-    res.write('</html>');
-    return res.end(); 
-}
+const routes = require('./routes');
 
-if(url === '/message' && method === 'POST'){
-    const body = [];
+console.log(routes.anotherText);
 
-    req.on('data', (chunk) => {
-        console.log(chunk);
-        body.push(chunk);
-        console.log(body);
-    });
-
-    return req.on('end', () => {
-        const parsedBody = Buffer.concat(body).toString();
-        console.log(parsedBody.split('='));
-        const message = parsedBody.split('=')[1];
-        fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-        });
-    });
-}
-res.write('<html>');
-res.write('<head ><h1 style="color:red">THE HEAD</h1></head>');
-res.write('<body>Hello World</body>');
-res.write('</html>');
-res.end();
-}).listen('3000');
+http.createServer(routes.handler).listen('3000');
