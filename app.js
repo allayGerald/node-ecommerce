ac1631cc405b6bd71b408ee88f99570266096d8d
allayDlareg <geraldallay@gmail.com>
2019-04-20 02:50:58 +0300

    Express routes

--- app.js
+++ app.js
@@ -2,12 +2,11 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First Middleware');
-    next(); // Allows calling next middleware
+app.use('/about', (req, res, next) => {
+   res.send('<h1>This is about page</h1>');
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     res.send('<h1>EXPRESS!!</h1>');
 })
 
