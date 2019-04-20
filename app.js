f39166eb6dda7cd0fd90aff95b5baacdc2463fbb
allayDlareg <geraldallay@gmail.com>
2019-04-20 03:23:50 +0300

    Parsing request

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.use('/about', (req, res, next) => {
-   res.send('<h1>This is about page</h1>');
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+   res.send('<form method="POST" action="/product"><input type="text" name="title"/><button type="submit">Submit</button"</form>');
+})
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
 app.use('/', (req, res, next) => {
