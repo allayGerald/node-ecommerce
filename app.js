b62a716119f2465bfbec3a364718b10738b847a7
allayDlareg <geraldallay@gmail.com>
2019-05-04 13:26:29 +0300

    Add Styling and Static Files

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(adminRoutes);
+
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/app');
 
-router.get('/add-product', (req, res, next) => {
+router.get('/admin/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
  
- router.post('/add-product', (req, res, next) => {
+ router.post('/admin/add-product', (req, res, next) => {
      console.log(req.body);
      res.redirect('/');
  })
