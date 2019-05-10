fd3c49b5d047ea210bd92ee6f63e43054d884c7e
allayDlareg <geraldallay@gmail.com>
2019-05-10 16:23:33 +0300

    add navigation links

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const productsController = require('../controllers/products');
 
 router.get('/admin/add-product', productsController.getAddProduct);
+router.get('/admin/products', productsController.getAdminProductsPage);
 
 router.post('/admin/add-product', productsController.postAddProduct);
 
--- routes/shop.js
+++ routes/shop.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const productsController = require('../controllers/products');
 
 router.get('/', productsController.getShopPage);
+router.get('/shop/cart', productsController.getCartPage);
+router.get('/shop/products', productsController.getProductsPage);
 
 module.exports = router;
