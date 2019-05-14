21cd8e1a44157e3c0db4f428ac081f2aaa452a0c
allayDlareg <geraldallay@gmail.com>
2019-05-14 17:58:52 +0300

    Reveting to head

---
+++ controllers/admin.js
@@ -0,0 +1,22 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/add-product',
+        { pageTitle: 'Add Product', path: 'admin/add-product' }
+    );
+}
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body);
+    product.save();
+    res.redirect('/');
+}
+
+
+exports.getAdminProductsPage = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('admin/products',
+            { pageTitle: 'Admin Products', products: products, path: 'admin/products' }
+        );
+    });
+}
