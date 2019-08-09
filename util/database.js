f9548d28bdc4aa080f01b616aa21c20f2fb2ad5c
allayDlareg <geraldallay@gmail.com>
2019-08-09 16:22:44 +0300

    set default timezone to +3:00

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('node', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
+    timezone: '+3:00',
     storage: './session.sql'
 });
 
